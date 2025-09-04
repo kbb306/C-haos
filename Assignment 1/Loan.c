15fa2e9f91ab57d1d467fff2107e00862aa199aa
kbb306 <kylerbbelshaw@gmail.com>
2025-09-04 14:09:22 -0600

    First commit

---
+++ Assignment 1/Loan.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <math.h>
+//Constants
+const int loanlength = 10;
+const double interest = .03;
+
+int main(){
+//Declare vars
+int salary, housing, car, groceries, utility;
+// Get input
+printf("Let's see how in debt you're gonna be.")
+printf("Enter the following numbers: \n");
+printf("1. Salary\n");
+scanf("%d", &salary);}
+printf("2. Housing\n");
+scanf("%d", &housing);
+printf("3. Car\n");
+scanf("%d", &car);
+printf("4. Groceries\n");
+scanf("%d", &groceries);
+printf("5. Utility\n");
+scanf("%d", &utility);
+// Actual math
+// Created by kylerbelshaw on 9/4/2025.
+//
