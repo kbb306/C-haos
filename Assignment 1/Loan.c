ef14efdfee888e2f1477492ad60553cec73d02c3
kylerbelshaw <kylerbbelshaw@gmail.com>
2025-09-04 16:53:20 -0700

    Added actual math

--- Assignment 1/Loan.c
+++ Assignment 1/Loan.c
@@ -21,5 +21,13 @@ scanf("%d", &groceries);
 printf("5. Utility\n");
 scanf("%d", &utility);
 // Actual math
+int expense = housing + car + groceries + utility;
+float leftover = ((salary/12) - expense)*.10;
+float payment = lefover * (interest/12);
+float denom = pow((1 + (interest/12)),12*loanlength);
+float mult = payment/(interest/12)
+float max = mult*(1/denom);
+printf("Your monthly payment is: " %.2f, payment);
+printf("Your maximum loan amount is: ",%.0f, max);
 // Created by kylerbelshaw on 9/4/2025.
 //
