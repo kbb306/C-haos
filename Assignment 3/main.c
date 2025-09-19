9fe23000b03fa5d5d0a5c6ec389eed6c86647053
kbb306 <kylerbbelshaw@gmail.com>
2025-09-19 12:38:16 -0600

    Simplified validation. Added comments and "paragraph breaks."

--- Assignment 3/main.c
+++ Assignment 3/main.c
@@ -7,9 +7,10 @@ int main() {
     int x = 0;
     while(x == 0) {
         x = open_account(0.00);
-    }
+    } // Initial account setup loop based on open_account()'s return code
+
     int choice = 0;
-    while(choice != 4) {
+    while(choice != 4) { // Trying to set the conditions for this loop baffled me until I looked back at my earlier work.
         printf("Welcome to the ATM of Evil!\n");
         printf("1. Deposit Balance\n");
         printf("2. Withdraw Balance\n");
@@ -22,7 +23,7 @@ int main() {
             double input;
             printf("Enter an amount to deposit: ");
             scanf("%lf",&input);
-            while (input < 0.00) {
+            while (input < 0.00) { //Validate you aren't depositing an amount < 0. This wouldn't be necessary if deposit() returned a status code
                 printf("Please enter a valid amount to deposit: ");
                 scanf("%lf",&input);
             }
@@ -30,29 +31,23 @@ int main() {
 
             }
         else if(choice == 2) {
-            int result = 3;
+            int result = 0;
             double input;
-            while (result > 1) {
+            while (result != 1) { // Validation loop based on withdraw()'s return vode
                 printf("Enter an amount to withdraw: ");
                 scanf("%lf",&input);
-                if (input < 0.00) {
-                    result = 3;
-                    printf("Invalid input, try again.\n");
-                }
-                else {
-                    result = withdraw(input);
-                    if (result == 0){printf("Withdrawl cancelled\n");};
-                }
-
+                result = withdraw(input);
             }
 
         }
         else if(choice == 3) {
-                check_balance();
+                check_balance(); // Nothing much here
         }
+
         else if(choice == 4) {
-            printf("Goodbye\n");
+            printf("Goodbye\n"); // So it doesn't print "Invalid option" when exiting.
         }
+
         else {
             printf("Invalid Option");
         }
