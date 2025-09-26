283cb7d47d4cad8afc2386d1ef34cb72e06fdeee
kbb306 <kylerbbelshaw@gmail.com>
2025-09-26 17:07:18 -0600

    Corrected function declaration.

--- Assignment 4/helper.h
+++ Assignment 4/helper.h
@@ -4,11 +4,11 @@
 
 #ifndef C_HAOS_HELPER_H
 #define C_HAOS_HELPER_H
-void fill_array_odd(int arr[], int size){}
-void fill_array_even(int arr[], int size){}
-void fill_array_random(int arr[], int size){}
-void print_array(int arr[], int size){}
-void sort_array_ascend(int arr[], int size){}
-void sort_array_descend(int arr[], int size){}
-void cancel(int arr[], int size){}
+void fill_array_odd(int arr[], int size);
+void fill_array_even(int arr[], int size);
+void fill_array_random(int arr[], int size);
+void print_array(int arr[], int size);
+void sort_array_ascend(int arr[], int size);
+void sort_array_descend(int arr[], int size);
+void cancel(int arr[], int size);
 #endif //C_HAOS_HELPER_H
