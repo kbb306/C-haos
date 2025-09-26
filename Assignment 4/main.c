ea35273888756cc832a846e1505f750c925da4d7
kbb306 <kylerbbelshaw@gmail.com>
2025-09-26 16:46:08 -0600

    Filled in fillmenu

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
-
-int fillmenu(){
+#include "helper.h"
+void (*fptr)(int[],int);
+int fillmenu(int arr[], int size){
     int exists = 0;
     int choice = 0;
     while (exists != 1) {
+        printf("Would you like to fill the array with...\n");
+        printf("1. Odd Numbers\n");
+        printf("2. Even Numbers\n");
+        printf("3. Random Numbers\n");
+        printf("Choose your choice: ");
+        scanf("%d", &choice);
         switch (choice) {
             case 1: {
-
+                fptr = &fill_array_odd;
+                break;
             }
+
+            case 2: {
+                fptr = &fill_array_even;
+                break;
+            }
+
+            case 3: {
+                fptr = &fill_array_random;
+                break;
+            }
+
         }
+    fptr(arr, size);
+    exists = 1;
     }
 }
 
 
-int printmenu(){}
 
-int sortmenu(){}
+
+int sortmenu(int arr[], int size){}
 
 int main() {
     int choice = 0;
+    int i = 0;
+    while (i <=1) {
+        printf("Please enter an array size");
+        scanf("%d",&i);
+        if (i <= 1) {
+            printf("Invalid Size\n");
+        }
+    }
+    int twelve[i];
     while (choice != 4) {
         printf("Welcome to the Array Display!\n");
         printf("1. Fill the array\n");
@@ -29,14 +59,14 @@ int main() {
         scanf("%d",&choice);
         switch (choice) {
             case 1: {
-                fillmenu();
+                fillmenu(twelve,i);
 
                 };
             case 2: {
-                printmenu();
+                printf(twelve);
             }
-                case 3: {
-                sortmenu();
+            case 3: {
+                sortmenu(twelve,i);
             }
             }
         }
