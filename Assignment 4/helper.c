4d833a1ba1b23a17203a6beb96ba12bfb501bc4b
kbb306 <kylerbbelshaw@gmail.com>
2025-09-26 17:27:45 -0600

    Created print_array and fill_array_* functions.

--- Assignment 4/helper.c
+++ Assignment 4/helper.c
@@ -1,5 +1,37 @@
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include "helper.h"
+#include <time.h>
 
+
+void print_array(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+    }
+}
+
+void fill_array_odd(int arr[], int size) {
+    int *j = arr;
+    for (int i = 0; i < size; i++) {
+        *j = 2*i - 1;
+        j ++;
+    }
+}
+
+void fill_array_even(int arr[], int size) {
+    int *j = arr;
+    for (int i = 0; i < size; i++) {
+        *j = 2*i;
+        j++;
+    }
+}
+
+void fill_array_random(int arr[], int size) {
+    int *j = arr;
+    srand(time(NULL));
+    for (int i = 0; i < size; i++) {
+        *j = rand() % 100;
+        j++;
+    }
 }
 
 //
