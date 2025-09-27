98017d2edb187e5cf69cb2c02312d8ed8fb1cf92
kbb306 <kylerbbelshaw@gmail.com>
2025-09-26 18:42:01 -0600

    Edited allowed range of even and odd numbers

--- Assignment 4/helper.c
+++ Assignment 4/helper.c
@@ -14,7 +14,7 @@ void print_array(int arr[], int size) {
 
 void fill_array_odd(int arr[], int size) {
     int *j = arr;
-    for (i = 0; i < size; i++) {
+    for (i = 1; i < size; i++) {
         *j = 2 * i - 1;
         j ++;
     }
@@ -22,7 +22,7 @@ void fill_array_odd(int arr[], int size) {
 
 void fill_array_even(int arr[], int size) {
     int *j = arr;
-    for (i = 0; i < size; i++) {
+    for (i = 2; i < size; i++) {
         *j = 2 * i;
         j++;
     }
@@ -41,6 +41,7 @@ void sort_array_ascend(int arr[], int size) {
     for (i = 0; i < size - 1; i++) {
         int *var1 = arr;
         int *var2 = var1++;
+        printf("var1 = %p, var2 = %p\n", var1, var2);
         int temp = 0;
         for (int j = i + 1; j < size; j++) {
             if (*var1 > *var2) {
@@ -60,6 +61,7 @@ void sort_array_descend(int arr[], int size) {
     for (i = 0; i < size - 1; i++) {
         int *var1 = arr;
         int *var2 = var1++;
+        printf("var1 = %p, var2 = %p\n", var1, var2);
         int temp = 0;
         for (int j = i + 1; j > size; j++) {
             if (*var1 > *var2) {
